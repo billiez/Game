f350d64e9a876f2d71bee3608bf747012feeb8a9
Billie <bbmerric@gmail.com>
2021-10-07 19:59:14 -0400

    Adding test class to demonstrate controllers.

---
+++ src/main/java/com/kenny/game/controller/Test.java
@@ -0,0 +1,13 @@
+package com.kenny.game.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class Test {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello, world!";
+    }
+}
